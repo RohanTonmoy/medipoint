8b612eb4cc58772d39d52c14aa5f3a7f43214328
RohanTonmoy <73402708+RohanTonmoy@users.noreply.github.com>
2024-01-16 11:50:44 -0500

    Add files via upload

--- lib/patient.dart
+++ lib/patient.dart
@@ -3,6 +3,12 @@ class Patient {
   String name;
   String email;
   String password;
+  String first = 'First';
+  String last = 'Last';
+  String m = '01';
+  String d = '01';
+  String y = '2000';
+  String desc = 'N/A';
 
   Patient(
       {this.id,
@@ -18,5 +24,29 @@ class Patient {
 
   Map<String, Object?> toMap() {
     return {'id': id, 'name': name, 'email': email, 'password': password};
+  }
+
+  void changef(String first) {
+    this.first = first;
+  }
+
+  void changel(String last) {
+    this.last = last;
+  }
+
+  void changem(String month) {
+    m = month;
+  }
+
+  void changed(String day) {
+    d = day;
+  }
+
+  void changey(String year) {
+    y = year;
+  }
+
+  void changedesc(String description) {
+    desc = description;
   }
 }
