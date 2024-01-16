ee087382d2ae485446c54aaa1d20374dd747407c
RohanTonmoy <73402708+RohanTonmoy@users.noreply.github.com>
2024-01-16 11:47:45 -0500

    Update schedulepage.dart

--- lib/schedulepage.dart
+++ lib/schedulepage.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:medipoint_new/applist.dart';
+import 'package:medipoint_new/appointmentdetails.dart';
+import 'package:medipoint_new/personalinfo.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'patient.dart';
+import 'package:medipoint_new/login.dart';
 
 class SchedulePage extends StatefulWidget {
   final Patient patient;
@@ -15,6 +19,7 @@ class _SchedulePageState extends State<SchedulePage> {
   late CalendarFormat _calendarFormat;
   late DateTime _focusedDay;
   late DateTime? _selectedDay;
+  int _currentIndex = 1;
 
   List<String> _scheduleDetails = [];
 
@@ -35,68 +40,104 @@ class _SchedulePageState extends State<SchedulePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text('Your Calendar'),
-        ),
-        body: Column(children: [
-          TableCalendar(
-            headerStyle: HeaderStyle(
-              formatButtonVisible: false,
-            ),
-            firstDay: DateTime.utc(2010, 10, 16),
-            lastDay: DateTime.utc(2030, 3, 14),
-            focusedDay: _focusedDay,
-            calendarFormat: _calendarFormat,
-            selectedDayPredicate: (day) {
-              return isSameDay(_selectedDay, day);
-            },
-            onDaySelected: (selectedDay, focusedDay) {
-              if (!isSameDay(_selectedDay, selectedDay)) {
-                setState(() {
-                  _selectedDay = selectedDay;
-                  _focusedDay = focusedDay;
-                });
-
-                /*
-                    Future<void> fetchSchedule(DateTime date) async{
-                      final response = await x.get(here we get the individual patient info in the sql database)
-                      );
-
-                      if(reponse == true){
-                        print(schedule)
-
-                      else{
-                        handle the null response here
-                      }
-                    }
-                  }
-
-                */
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Your Calendar'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+        TableCalendar(
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+          ),
+          firstDay: DateTime.utc(2010, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          focusedDay: _focusedDay,
+          calendarFormat: _calendarFormat,
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            if (!isSameDay(_selectedDay, selectedDay)) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
               }
-            },
-            onPageChanged: (focusedDay) {
-              _focusedDay = focusedDay;
+              );
+              // Handle day selection logic
+              
+              Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    AppList(patient: widget.patient, dateTime: selectedDay)));
+            }
+          },
+          onPageChanged: (focusedDay) {
+            _focusedDay = focusedDay;
+          },
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _scheduleDetails.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_scheduleDetails[index]),
+              );
             },
           ),
-          Expanded(
-              child: ListView.builder(
-                  itemCount: _scheduleDetails.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(_scheduleDetails[index]),
-                    );
-                  }))
-        ]),
-        bottomNavigationBar: BottomNavigationBar(items: [
+        ),
+      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+          _navigateToPage(value, context);
+        },
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
             label: "Personal Info",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.calendar_today),
             label: "Schedule",
-          )
-        ]));
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.details),
+            label: "Appointment Details",
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _navigateToPage(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) => PersonalInfo(patient: widget.patient)));
+        break;
+      case 1:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => SchedulePage(patient: widget.patient)),
+        );
+        break;
+      case 2:
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    AppointmentDetails(patient: widget.patient)));
+        break;
+    }
   }
 }
