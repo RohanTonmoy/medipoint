e57356307d56cc741810bd40c5effc5113ad106a
RohanTonmoy <73402708+RohanTonmoy@users.noreply.github.com>
2024-01-16 11:54:36 -0500

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'databasehelper.dart';
+import 'userhelper.dart';
 import 'patient.dart';
 import 'schedulepage.dart';
 import 'loginError.dart';
@@ -16,111 +16,127 @@ class _Login extends State<LogIn> {
   String inputEmail = '';
   String inputPassword = '';
 
-  Future<void> checkCredentials() async {
-    /*here check in the backend if email and password matches
-
-      if (response == success){
-        Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => SchedulePage(email: email)),
-
-      else{
-        return)("Invalid Password. Try Again")
-      }
-    );
-        
-      }
-  */
-  }
+  
 
   late DatabaseHelper dbHelper;
+
+  @override
   void initState() {
     super.initState();
-    this.dbHelper = DatabaseHelper();
-    this.dbHelper.initDB().whenComplete(() async {
+    dbHelper = DatabaseHelper();
+    dbHelper.initDB().whenComplete(() {
       setState(() {});
     });
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
+          title: Text(
+            widget.title,
+            style: const TextStyle(
+              fontFamily: 'TimesNewRoman',
+              fontSize: 17,
+            ),
+          ),
         ),
-        body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(height: 40),
-                Text("Enter your email:",
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    )),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-                  child: TextField(
-                    onChanged: (value) => inputEmail = value,
-                    textAlign: TextAlign.center,
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      hintText: 'Email',
-                    ),
-                  ),
-                ),
-                SizedBox(height: 40),
-                Text('Enter your password:',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    )),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      hintText: 'Password',
-                    ),
-                  ),
-                ),
-                SizedBox(height: 30),
-                ElevatedButton(
-                  child: Text('Submit',
+        body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/hospitalbg.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 40),
+                    const Text(
+                      "Enter your email:",
                       style: TextStyle(
-                        fontSize: 20,
-                      )),
-                  onPressed: () {
-                    print(dbHelper.loginPatient('email', 'password'));
-                    int response =
-                        dbHelper.loginPatient(inputEmail, inputPassword);
-                    Patient patient =
-                        dbHelper.getPatient(inputEmail, inputPassword);
-                    if (response == -1) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                SchedulePage(patient: patient)),
-                      );
-                    } else {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LogInError(title: '')),
-                      );
-                    }
+                        fontFamily: 'TimesNewRoman',
+                        fontSize: 17,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                      child: TextField(
+          
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          hintText: 'Email',
+                          
+                        ),
+                        onChanged: (value) => inputEmail = value,
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    const Text(
+                      'Enter your password:',
+                      style: TextStyle(
+                        fontFamily: 'TimesNewRoman',
+                        fontSize: 17,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          hintText: 'Password',
+                        ),
+                        onChanged: (value) => inputPassword = value,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      child: const Text(
+                        'Submit',
+                        style: TextStyle(
+                          fontFamily: 'TimesNewRoman',
+                          fontSize: 17,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        print(dbHelper.loginPatient('email', 'password'));
+                        int response =
+                            dbHelper.loginPatient(inputEmail, inputPassword);
+                        Patient patient =
+                            dbHelper.getPatient(inputEmail, inputPassword);
+                        if (response == -1) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    SchedulePage(patient: patient)),
+                          );
+                        } else {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const LogInError(title: '')),
+                          );
+                        }
 
-                    //Check to see if credentials match database
-                  },
-                ),
-              ]),
-        ));
+                        //Check to see if credentials match database
+                      },
+                    ),
+                  ]),
+            )));
   }
 }
