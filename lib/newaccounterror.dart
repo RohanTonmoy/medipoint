eb259fac2f932281c394c3c30fd5c81cc114878d
RohanTonmoy <73402708+RohanTonmoy@users.noreply.github.com>
2024-01-17 02:42:54 -0500

    Update newaccounterror.dart

--- lib/newaccounterror.dart
+++ lib/newaccounterror.dart
@@ -30,8 +30,8 @@ class _NewAccountErrorState extends State<NewAccountError> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text('Create New Account'),
       ),
       body: Container(
         decoration: const BoxDecoration(
@@ -53,94 +53,113 @@ class _NewAccountErrorState extends State<NewAccountError> {
                 )),
               const SizedBox(height: 40),
               // Name input
-              const Text("Enter your name:",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                )),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-                child: TextField(
-                  onChanged: (value) => userName = value,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    hintText: 'Name',
-                  ),
-                ),
-              ),
-              SizedBox(height: 40),
-              // Email input
-              const Text("Enter your email:",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                )),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-                child: TextField(
-                  onChanged: (value) => userEmail = value,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    hintText: 'Email',
-                  ),
-                ),
-              ),
-              SizedBox(height: 40),
-              // Password input
-              const Text('Enter your password:',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                )),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-                child: TextField(
-                  onChanged: (value) => userPassword = value,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    hintText: 'Password',
-                    icon: Icon(Icons.lock),
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-              // Submit button
-              ElevatedButton(
-                child: const Text('Submit',
-                  style: TextStyle(
-                    fontSize: 20,
-                  )),
-                onPressed: () {
-                  if (dbHelper.loginPatient(userEmail, userPassword) != -1) {
-                    Patient newPatient = Patient(
-                        name: userName,
-                        email: userEmail,
-                        password: userPassword);
-                    dbHelper.insertPatient(newPatient);
-                    dbHelper.retrievePatients().then(
-                          (value) => value.forEach((e) => print(e.toMap())),
-                        );
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SchedulePage(patient: newPatient),
-                        ));
-                  } else {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              const NewAccountError(title: '')),
-                    );
-                  }
-                },
-              ),
+              Text("Enter your name:",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 10),
+                      child: TextField(
+          
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          hintText: 'Name',
+                          filled: true
+                      ),
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    Text("Enter your email:",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 10),
+                      child: TextField(
+          
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          hintText: 'Email',
+                          filled: true
+                      ),
+                      )
+                    ),
+                    SizedBox(height: 40),
+                    Text('Enter your password:',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 10),
+                      child: TextField(
+                        onChanged: (value) => userPassword = value,
+                        
+                        decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            hintText: 'Password',
+                            filled: true,
+                            
+                            icon: const Padding(
+                              padding: const EdgeInsets.only(top: 15.0),
+                              child: const Icon(Icons.lock),
+                            )),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      child: Text('Submit',
+                          style: TextStyle(
+                            fontSize: 20,
+                          )),
+                      onPressed: () {
+                        // Patient newPatient = Patient(
+                        //     name: 'sample patient',
+                        //     email: email,
+                        //     password: password);
+                        // dbHelper.retrievePatients().then(
+                        //       (value) => value.forEach((e) => print(e.toMap())),
+                        //     );
+                        if (dbHelper.loginPatient(userEmail, userPassword) ==
+                            -1) {
+                          Patient newPatient = Patient(
+                              name: userName,
+                              email: userEmail,
+                              password: userPassword);
+                          dbHelper.insertPatient(newPatient);
+
+                          dbHelper.retrievePatients().then(
+                                (value) =>
+                                    value.forEach((e) => print(e.toMap())),
+                              );
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    SchedulePage(patient: newPatient),
+                              ));
+                        } else {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const NewAccountError(title: '')),
+                          );
+                        }
+
+                        //check to see if credentials match database
+                      },
+                    ),
             ],
           ),
         ),
